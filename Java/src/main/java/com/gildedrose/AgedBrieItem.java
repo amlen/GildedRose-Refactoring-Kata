57589c3e996c6ad69da15fd5e79e8814f9191282
yass.d@outlook.com <Yassine--1991>
2024-04-02 11:57:04 +0200

    Refactor: extract increaseQuality method

--- Java/src/main/java/com/gildedrose/AgedBrieItem.java
+++ Java/src/main/java/com/gildedrose/AgedBrieItem.java
@@ -1,17 +1,22 @@
 package com.gildedrose;
 
-public class AgedBrieItem implements ItemUpdater{
+public class AgedBrieItem implements ItemUpdater {
     public static final String NAME = "Aged Brie";
+
     @Override
     public void update(Item item) {
         item.sellIn--;
 
         if (item.quality < MAX_QUALITY) {
-            item.quality++;
+            increaseQuality(item, 1);
         }
 
         if (item.sellIn < 0 && item.quality < MAX_QUALITY) {
-            item.quality++;
+            increaseQuality(item, 1);
         }
+    }
+
+    private void increaseQuality(Item item, int i) {
+        item.quality = item.quality + i;
     }
 }
